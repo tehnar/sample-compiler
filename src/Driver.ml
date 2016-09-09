a8a878c43cde76983a632765520d007a51e15d6f
dboulytchev <dboulytchev@gmail.com>
2016-09-09 12:21:08 +0300

    Eval-Run-DSL

--- src/Driver.ml
+++ src/Driver.ml
@@ -1,1 +1,53 @@
-let _ = ()
+open Expr
+
+(*
+read (x);
+read (y);
+z := x * x;
+write (z+y)
+*)
+let p =
+  Seq (
+      Read "x",
+      Seq (
+          Read "y",
+          Seq (
+              Assign ("z", Mul (Var "x", Var "x")),
+              Write (Add (Var "z", Var "y"))
+          )
+      )
+    )
+
+let _ =
+  let [r] = run [3; 4] p in
+  Printf.printf "%d\n" r
+
+let ( !! )       = (!)
+let ( !  ) x     = Var x
+let ( $  ) n     = Const n
+let ( +  ) e1 e2 = Add (e1, e2)
+let ( *  ) e1 e2 = Mul (e1, e2)
+
+let skip     = Skip
+let (:=) x e = Assign (x, e)
+let read x   = Read x
+let write x  = Write x
+let (|>) l r = Seq (l, r)
+
+(*
+read (x);
+read (y);
+z := x * x;
+write (z+y)
+*)
+
+let p =
+  read "x" |>
+  read "y" |>
+  ("z" := !"x" * !"x") |>
+  write (!"z" + !"y")  
+
+let _ =
+  let [r] = run [3; 4] p in
+  Printf.printf "%d\n" r
+                
--- src/Expr.ml
+++ src/Expr.ml
@@ -1,1 +1,35 @@
-let _ = ()
+type expr =
+  | Const of int
+  | Var   of string
+  | Add   of expr * expr
+  | Mul   of expr * expr
+
+let rec eval state expr =
+  match expr with
+  | Const  n     -> n
+  | Var    x     -> state x
+  | Add   (l, r) -> eval state l + eval state r
+  | Mul   (l, r) -> eval state l * eval state r
+
+type stmt =
+  | Skip
+  | Read   of string
+  | Write  of expr
+  | Assign of string * expr
+  | Seq    of stmt * stmt
+                       
+let run input stmt =
+  let rec run' ((state, input, output) as c) stmt =
+    let state' x = List.assoc x state in
+    match stmt with
+    | Skip          -> c
+    | Seq    (l, r) -> run' (run' c l) r
+    | Assign (x, e) -> ((x, eval state' e) :: state, input, output)
+    | Write   e     -> (state, input, output @ [eval state' e])
+    | Read    x     ->
+       let y::input' = input in
+       ((x, y) :: state, input', output)
+  in
+  let (_, _, result) = run' ([], input, []) stmt in
+  result
+    
